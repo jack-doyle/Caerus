981b2edc5c3da0adb0e8d4517e343f7007001c5e
Jack Doyle <doylejj@tcd.ie>
2015-07-02 14:08:53 +0000

    Commented functions.php

--- includes/functions.php
+++ includes/functions.php
@@ -9,21 +9,28 @@ function get_styles() {
 	echo($stylesheet_path = on_index() !== false ? "" : "../");
 }
 
+// Function to get the correct url for each of the menu items.
 function get_url($name) {
+	// If on the index page,
 	if(on_index()) {
+		// look in the 'php/' directory for all the pages,
 		if($name !== "index") {
 			$path = "php/";
+		// except the index page, which is in this directory.
 		} else {
 			$path = "";
 		}
+	// else if already in the php directory,
 	} else {
+		// look in this directory for all the pages,
 		if($name !== "index") {
 			$path = "";
+		// except the index page which is in the parent directory (root directory).
 		} else {
 			$path = "../";
 		}
 	}
-
+	
 	echo($path);
 }
 	
